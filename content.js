0fceef440ca2a993b3a77b2f489caf6b98c3b6b5
wh1t3dead <wh1t3dead@gmail.com>
2026-03-17 22:22:52 +0300

    Update content.js

--- content.js
+++ content.js
@@ -78,6 +78,7 @@
           title: "cs2",
           type: "mp4",
           url: "videos/edit_cs_2.mp4",
+          volume: 0.7,
           duration: "1-2 минуты",
           label: "",
           description: ""
@@ -87,7 +88,7 @@
           type: "mp4",
           url: "videos/heronwater_trapozoid.mp4",
           duration: "5-15 сек",
-          volume: 0.6,
+          volume: 0.4,
           label: "",
           description: ""
         },
